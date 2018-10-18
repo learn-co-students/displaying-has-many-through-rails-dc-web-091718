19db1cd1cc72b61f08a1df499f5e08a1b7f5befd
pasolre95kim <pasolre95kim@gmail.com>
2018-10-18 17:04:52 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+  #user instances respond to method called 'posts'
+  #=> return collection of posts that share a comment with the user
 end
